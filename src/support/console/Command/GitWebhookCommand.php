26e3d45a69901df3a47f0bbaf628cda0556d5e6a
Иван Зорин <local.zet@yandex.ru>
2023-02-27 11:18:17 +0300

    Create GitWebhookCommand.php

---
+++ src/support/console/Command/GitWebhookCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace support\console\Command;
+
+use support\console\Input\InputInterface;
+use support\console\Output\OutputInterface;
+
+class GitWebhookCommand extends Command
+{
+    protected static $defaultName = 'git:webhook|git-webhook';
+    protected static $defaultDescription = 'Добавить Route для GitHub WebHook';
+
+    /**
+     * @return void
+     */
+    protected function configure()
+    {
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {        
+        $file = base_path() . "/config/route.php";
+        $conf = <<<EOF
+
+        Triangle\Engine\Route::any('/.githook', function(support\Request \$request) {
+            \$output = exec('cd ' . base_path() . ' && sudo git pull');
+            return responseJson(\$output);
+        });
+        EOF;
+
+        $fstream = fopen($file, 'w');
+        fwrite($fstream, $conf);
+        fclose($fstream);
+
+        $output->writeln("<info>Route добавлен. Настройте репозиторий на отправку Webhook на {домен}/.githook</>");
+        return self::SUCCESS;
+    }
+}
