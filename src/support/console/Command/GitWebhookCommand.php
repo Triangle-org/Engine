d64870eb443bfcba1dcd25d4632066d78f52d838
Иван Зорин <local.zet@yandex.ru>
2023-02-27 11:24:12 +0300

    Update GitWebhookCommand.php

--- src/support/console/Command/GitWebhookCommand.php
+++ src/support/console/Command/GitWebhookCommand.php
@@ -33,7 +33,7 @@ class GitWebhookCommand extends Command
         });
         EOF;
 
-        $fstream = fopen($file, 'w');
+        $fstream = fopen($file, 'a');
         fwrite($fstream, $conf);
         fclose($fstream);
 
