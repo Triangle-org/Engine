5a3cbb512c2a192a3a69aac4a13da78aa4267a23
Иван Зорин <33757217+localzet@users.noreply.github.com>
2023-04-12 22:57:26 +0300

    Fix

--- src/Install.php
+++ src/Install.php
@@ -63,17 +63,17 @@ class Install
     public static function installByRelation()
     {
         foreach (static::$pathRelation as $source => $dest) {
-            if ($pos = strrpos($dest, '/')) {
-                $parentDir = base_path() . '/' . substr($dest, 0, $pos);
-                if (!is_dir($parentDir)) {
-                    mkdir($parentDir, 0777, true);
+            if ($pos = \strrpos($dest, '/')) {
+                $parentDir = \base_path() . '/' . \substr($dest, 0, $pos);
+                if (!\is_dir($parentDir)) {
+                    \mkdir($parentDir, 0777, true);
                 }
             }
             $sourceFile = __DIR__ . "/$source";
-            copy_dir($sourceFile, base_path() . "/$dest", true);
+            \copy_dir($sourceFile, \base_path() . "/$dest", true);
             echo "Создан $dest\r\n";
-            if (is_file($sourceFile)) {
-                @unlink($sourceFile);
+            if (\is_file($sourceFile)) {
+                @\unlink($sourceFile);
             }
         }
     }
