5aa63234d1a9263fa404ee1660b92b0acb9c93a6
Ivan Zorin <creator@localzet.ru>
2023-04-14 19:47:27 +0300

    Update Install.php

--- src/Install.php
+++ src/Install.php
@@ -64,13 +64,13 @@ class Install
     {
         foreach (static::$pathRelation as $source => $dest) {
             if ($pos = \strrpos($dest, '/')) {
-                $parentDir = \base_path() . '/' . \substr($dest, 0, $pos);
+                $parentDir = base_path() . '/' . \substr($dest, 0, $pos);
                 if (!\is_dir($parentDir)) {
                     \mkdir($parentDir, 0777, true);
                 }
             }
             $sourceFile = __DIR__ . "/$source";
-            \copy_dir($sourceFile, \base_path() . "/$dest", true);
+            copy_dir($sourceFile, base_path() . "/$dest", true);
             echo "Создан $dest\r\n";
             if (\is_file($sourceFile)) {
                 @\unlink($sourceFile);
