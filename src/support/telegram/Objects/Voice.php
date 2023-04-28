10fa5d59fbbce858bf2f1c98dd507cd1e218267a
Иван Зорин <33757217+localzet@users.noreply.github.com>
2023-04-28 11:16:33 +0300

    Update Voice.php

--- src/support/telegram/Objects/Voice.php
+++ src/support/telegram/Objects/Voice.php
@@ -7,11 +7,13 @@ namespace support\telegram\Objects;
  *
  * @link https://core.telegram.org/bots/api#voice
  *
- * @property string      $fileId         Unique identifier for this file.
- * @property string      $fileUniqueId   Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
- * @property int         $duration       Duration of the audio in seconds as defined by sender.
- * @property string|null $mimeType       (Optional). MIME type of the file as defined by sender.
- * @property int|null    $fileSize       (Optional). File size.
+ * @property string         $fileId         Identifier for this file, which can be used to download or reuse the file.
+ * @property string         $fileUniqueId   Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
+ * @property int            $duration       Duration of the audio in seconds as defined by sender.
+ * @property string|null    $mimeType       (Optional) MIME type of the file as defined by sender.
+ * @property int|null       $fileSize       (Optional) File size in bytes. It can be bigger than 2^31 and some programming 
+ *                                                      languages may have difficulty/silent defects in interpreting it. But it has at most 52 significant bits, 
+ *                                                      so a signed 64-bit integer or double-precision float type are safe for storing this value.
  */
 class Voice extends BaseObject
 {
