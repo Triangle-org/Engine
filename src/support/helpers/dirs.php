9a8cb7500c8653fcea20e773718090cb740d9606
Ivan Zorin <creator@localzet.com>
2023-11-07 22:33:48 +0300

    Update dirs.php

--- src/support/helpers/dirs.php
+++ src/support/helpers/dirs.php
@@ -13,11 +13,9 @@ function copy_dir(string $source, string $dest, bool $overwrite = false): void
         if (!is_dir($dest)) {
             mkdir($dest);
         }
-        $files = scandir($source);
+        $files = scandir($source, SCANDIR_SORT_NONE)[2:];
         foreach ($files as $file) {
-            if ($file !== "." && $file !== "..") {
-                copy_dir("$source/$file", "$dest/$file");
-            }
+            copy_dir("$source/$file", "$dest/$file");
         }
     } else if (file_exists($source) && ($overwrite || !file_exists($dest))) {
         copy($source, $dest);
@@ -35,10 +33,8 @@ function scan_dir(string $basePath, bool $withBasePath = true): array
     if (!is_dir($basePath)) {
         return [];
     }
-    $paths = array_diff(scandir($basePath), array('.', '..')) ?: [];
-    return $withBasePath ? array_map(static function ($path) use ($basePath) {
-        return $basePath . DIRECTORY_SEPARATOR . $path;
-    }, $paths) : $paths;
+    $paths = scandir($basePath, SCANDIR_SORT_NONE)[2:];
+    return $withBasePath ? array_map(fn($path) => $basePath . DIRECTORY_SEPARATOR . $path, $paths) : $paths;
 }
 
 /**
@@ -51,9 +47,30 @@ function remove_dir(string $dir): bool
     if (is_link($dir) || is_file($dir)) {
         return unlink($dir);
     }
-    $files = array_diff(scandir($dir), array('.', '..'));
+    $files = scandir($dir, SCANDIR_SORT_NONE)[2:];
     foreach ($files as $file) {
         (is_dir("$dir/$file") && !is_link($dir)) ? remove_dir("$dir/$file") : unlink("$dir/$file");
     }
     return rmdir($dir);
+}
+
+/**
+ * Create directory
+ * @param string $dir
+ * @return bool
+ */
+function create_dir(string $dir): bool
+{
+    return mkdir($dir);
+}
+
+/**
+ * Rename directory
+ * @param string $oldName
+ * @param string $newName
+ * @return bool
+ */
+function rename_dir(string $oldName, string $newName): bool
+{
+    return rename($oldName, $newName);
 }
