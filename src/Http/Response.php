c0c4677a72cd4a189dcef669405d0fea314c21c2
Ivan Zorin <creator@localzet.com>
2023-11-15 13:09:42 +0300

    Update Response.php

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -80,7 +80,7 @@ class Response extends \localzet\Server\Protocols\Http\Response
     protected function notModifiedSince(string $file): bool
     {
         $ifModifiedSince = App::request()->header('if-modified-since');
-        if ($ifModifiedSince === null || !($mtime = filemtime($file))) {
+        if ($ifModifiedSince === null || !is_file($file) || !($mtime = filemtime($file))) {
             return false;
         }
         return $ifModifiedSince === gmdate('D, d M Y H:i:s', $mtime) . ' GMT';
